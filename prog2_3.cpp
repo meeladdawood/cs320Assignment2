653c738a9c7a1f1998a06eeddb2f430833d7bb16
Meelad <mdawood2@sdsu.edu>
2017-06-15 12:55:29 -0700

    made the prog2-3 file

---
+++ prog2_3.cpp
@@ -0,0 +1,31 @@
+nclude<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"Tokenizer.hpp"
+
+
+int main(int argc, char *argv[]){
+	Tokenizer tokens;
+
+	int times = 0;
+	printf("Assignment #2-3, Meelad Dawood, mdawood2@sdsu.edu\n");
+	times = atoi(argv[1]);
+	
+
+
+	for(int i = 0; i < times ; i++){
+		tokens.GetTokens();
+		if(tokens.strings.size()== 1){
+			printf("STR\n");
+			
+		}
+		if(tokens.strings.size()== 2){
+                        printf("STR INT\n");
+                }
+
+	}
+
+}
+
+
+
