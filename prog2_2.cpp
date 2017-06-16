684d7bd1492a8f142b7d7e0e876191e8d988869a
Meelad <mdawood2@sdsu.edu>
2017-06-15 17:14:18 -0700

    fixed the quit issue

--- prog2_2.cpp
+++ prog2_2.cpp
@@ -12,13 +12,17 @@ int main(int argc, char *argv[]){
 	times = atoi(argv[1]);
 	
 
-
+	string leave = "quit";
 	for(int i = 0; i < times ; i++){
 		tokens.GetTokens();
-		if(tokens.strings.size()== 1){
-			printf("STR\n");
+		if(tokens.strings.size()== 1 && strcasecmp(tokens.strings.at(0).c_str(),leave.c_str())==0){
+			exit(0);
 			
+		} else if(tokens.strings.size() ==1){
+			printf("STR");
 		}
+
+		
 		if(tokens.strings.size()== 2){
                         printf("STR INT\n");
                 }
