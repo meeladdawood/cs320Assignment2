d71163620c6c4e3c2f41e413aa4df879d340d3bc
Meelad <mdawood2@sdsu.edu>
2017-06-15 17:37:33 -0700

    changed alot in prog2_3 but it's working

--- prog2_3.cpp
+++ prog2_3.cpp
@@ -2,26 +2,33 @@
 #include<stdlib.h>
 #include<string.h>
 #include"Tokenizer.hpp"
+#include "Stack.cpp"
 
 
 int main(int argc, char *argv[]){
 	Tokenizer tokens;
+	Stack<int> stack;
 
 	int times = 0;
 	printf("Assignment #2-3, Meelad Dawood, mdawood2@sdsu.edu\n");
 	times = atoi(argv[1]);
 	
 
-
+	string leave = "quit";
 	for(int i = 0; i < times ; i++){
-		tokens.GetTokens();
-		if(tokens.strings.size()== 1){
-			printf("STR\n");
+		vector<string> *ptr = tokens.GetTokens();
+		if(ptr->size()== 1 && strcasecmp(ptr->at(0).c_str(),leave.c_str())==0){
+			exit(0);
+			
+		} else if(ptr->size() ==1 && strcmp(ptr->at(0).c_str(),"pop")==0){
+			stack.Pop();			
+		}
+
+		
+		if(ptr->size()== 2 && strcmp(ptr->at(0).c_str(),"push")==0){
+                	stack.Push(atoi(ptr->at(1).c_str()));
 			
 		}
-		if(tokens.strings.size()== 2){
-                        printf("STR INT\n");
-                }
 
 	}
 
